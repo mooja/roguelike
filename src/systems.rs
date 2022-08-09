d73e6af430c02ca75d528dbd9debd76d27aa1b0f
Max Shkurygin <max.atreides@gmail.com>
2022-08-09 15:52:46 -0400

    only add viewshed to revealed tiles if the entity is player

--- src/systems.rs
+++ src/systems.rs
@@ -3,7 +3,9 @@ use std::fmt::Write;
 use super::components::{Player, Position, Viewshed};
 use super::map;
 use rltk::{field_of_view, Algorithm2D, GameState, Point};
-use specs::{Join, ReadExpect, ReadStorage, System, SystemData, WriteExpect, WriteStorage};
+use specs::{
+    Entities, Join, ReadExpect, ReadStorage, System, SystemData, WriteExpect, WriteStorage,
+};
 
 pub struct VisibilitySystem;
 
@@ -12,11 +14,13 @@ impl<'a> System<'a> for VisibilitySystem {
         WriteStorage<'a, Viewshed>,
         ReadStorage<'a, Position>,
         WriteExpect<'a, map::GameMap>,
+        ReadStorage<'a, Player>,
+        Entities<'a>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (mut viewshed, pos, mut map) = data;
-        for (mut viewshed, pos) in (&mut viewshed, &pos).join() {
+        let (mut viewshed, pos, mut map, player, e) = data;
+        for (e, mut viewshed, pos) in (&e, &mut viewshed, &pos).join() {
             if !viewshed.dirty {
                 break;
             }
@@ -27,9 +31,12 @@ impl<'a> System<'a> for VisibilitySystem {
                 .visible_tiles
                 .retain(|p| p.x >= 0 && p.x < map.w as i32 && p.y >= 0 && p.y < map.h as i32);
 
-            for vt in &viewshed.visible_tiles {
-                let idx = map.point2d_to_index(*vt);
-                map.revealed_tiles[idx] = true;
+            let p = player.get(e);
+            if let Some(p) = p {
+                for vt in &viewshed.visible_tiles {
+                    let idx = map.point2d_to_index(*vt);
+                    map.revealed_tiles[idx] = true;
+                }
             }
         }
     }
